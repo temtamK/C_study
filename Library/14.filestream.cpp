0334e86b015fcc119fd70f518a867e8befc3edba
temtamK <temtamk@kakao.com>
2020-05-08 23:08:13 +0900

    [ADD]file

--- Library/14.filestream.cpp
+++ Library/14.filestream.cpp
@@ -2,17 +2,15 @@
 
 // int main()
 // {
-//     //FILE은 구조체
 //     FILE *fp = NULL;
 //     fp = fopen("test.txt", "w");
 //     if (fp != NULL)
 //     {
-//         fprintf(fp, "hello world\n");
+//         putc('a', fp);
+//         putc('b', fp);
+//         putc('c', fp);
+
 //         fclose(fp);
-//     }
-//     else
-//     {
-//         printf("실패!");
 //     }
 // }
 
@@ -22,10 +20,14 @@ int main()
     fp = fopen("test.txt", "r");
     if (fp != NULL)
     {
-        char str1[16] = {};
-        char str2[16] = {};
-        fscanf(fp, "%s%s", str1, str2);
-        printf("%s %s", str1, str2);
+        int c = 3;
+        c = getc(fp);
+        printf("%c", c);
+        c = getc(fp);
+        printf("%c", c);
+        c = getc(fp);
+        printf("%c", c);
+
         fclose(fp);
     }
 }
